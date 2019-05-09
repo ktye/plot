d7e083f03c724453637007fb724119dc7202d145
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2019-05-09 18:11:51 +0200

    ui: buffer image for forced redraws
    Forced redraws may happen, requested by another widget.
    Only the plot ui itself known, if it needs to redraw everything,
    which may take long for lots of data.
    Otherwise it can take a shortcut, copying from a buffer.

--- ui.go
+++ ui.go
@@ -8,9 +8,10 @@ import (
 // This file provides a basic widget implementation for for github.com/ktye/ui.
 
 type UI struct {
-	Plots
-	Hi   []HighlightID
+	p    Plots
+	hi   []HighlightID
 	rect image.Rectangle
+	save *image.RGBA
 }
 
 func NewUI(p Plots) *UI {
@@ -19,19 +20,33 @@ func NewUI(p Plots) *UI {
 	return &u
 }
 func (u *UI) SetPlots(p Plots) {
-	u.Plots = p
+	u.p = p
+	u.hi = nil
 	u.rect = image.Rectangle{}
-	u.Hi = nil
+	u.save = nil
+}
+func (u *UI) Highlight(hi []HighlightID) {
+	u.hi = hi
+	u.save = nil
 }
 func (u *UI) Draw(dst *image.RGBA, force bool) {
-	if u.rect != dst.Rect || force {
+	if force && u.save != nil && u.save.Rect == dst.Rect {
+		// Forced draw, but unchanged plot.
+		draw.Draw(dst, dst.Rect, u.save, dst.Rect.Min, draw.Src)
+		return
+	}
+	if u.rect != dst.Rect {
 		u.rect = dst.Rect
 		size := u.rect.Size()
-		if h, err := u.IPlots(size.X, size.Y); err == nil {
-			if im := Image(h, u.Hi, size.X, size.Y); im != nil {
-				draw.Draw(dst, u.rect, im, image.ZP, draw.Src)
+		if h, err := u.p.IPlots(size.X, size.Y); err == nil {
+			u.save = Image(h, u.hi, size.X, size.Y).(*image.RGBA)
+			if u.save == nil {
+				u.save = image.NewRGBA(dst.Rect)
+			} else {
+				u.save.Rect = u.save.Rect.Add(dst.Rect.Min)
 			}
 		}
+		draw.Draw(dst, dst.Rect, u.save, dst.Rect.Min, draw.Src)
 	}
 }
 func (u *UI) Mouse(pos image.Point, but int, dir int, mod uint32) int { return 0 }
