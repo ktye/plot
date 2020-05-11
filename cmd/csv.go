9f163086c5a38743f9e7a072c76dbf41fedcaad1
U-DDA01\csd80117 <ktye@users.noreply.github.com>
2020-05-11 12:51:15 +0200

    cmd/csv.go

--- cmd/csv.go
+++ cmd/csv.go
@@ -12,18 +12,18 @@ import (
 
 const help = `(ktye/plot/cmd/csv.go)
 
-csv < data.csv
+csv FORMAT < data.csv
+
+csv ,fff            # use , as separator(first)  default spaces/tabs
+csv h2f5            # skip 2 header lines..
 
 csv fff             # 3 float columns, ignore remaining
-csv f*              # all remaining floats
-csv f3              # same
-csv fz              # float abs angle, e.g 1.2 3.0 40 -> 1.2 3.0a40
-csv f@              # float abs angle,                   1.2 3.0@40
-csv f-f             # skip
-csv h2f5            # skip 2 header lines..
-csv nf              # missing/parse as 0n instead of trap
-
-csv ,fff            # separator is , (default spaces/tabs)
+csv f*              # * repeat last format for all remaining cols
+csv f3              # repeat last format 3 times (f3 is fff)
+csv a               # abs angle, e.g 3.0 40 -> 3.0a40
+csv @               # abs angle,               3.0@40
+csv f-f             # - skip 2nd
+csv nf              # n missing/parse error as 0n instead of trap
 
 
 f(float) a|@(polar) s(string) q("quoted") ` + "`" + `(` + "`" + `symbol)
@@ -38,12 +38,12 @@ var f []byte
 
 func main() {
 	if len(os.Args) != 2 {
-		fatal(help)
+		fatal("wrong number of args\n" + help)
 	}
 	a := os.Args[1]
 	if len(a) > 0 {
 		s := ",; \t"
-		if i := strings.Index(a, s); i != -1 {
+		if i := strings.IndexByte(s, a[0]); i != -1 {
 			sep, a = s[i], a[1:]
 		}
 	}
@@ -51,24 +51,33 @@ func main() {
 		nan, a = true, a[1:]
 	}
 	for len(a) > 0 {
+		shift := func(c byte) {
+			f = append(f, c)
+			a = a[1:]
+		}
 		switch a[0] {
+		case 'h':
+			shift('h')
 		case 'f':
-			f = append(f, 'f')
-		case 'z', 'a':
-			f = append(f, 'z')
+			shift('f')
+		case 'a', 'z':
+			shift('a')
 		case '@':
-			f = append(f, '@')
+			shift('@')
 		case '-':
-			f = append(f, '-')
+			shift('-')
 		case 's', 'c':
-			f = append(f, 's')
+			shift('s')
 		case 'q':
-			f = append(f, 'q')
+			shift('q')
 		case '`':
-			f = append(f, '`')
+			shift('`')
 		case '*':
 			if len(f) == 0 {
-				fatal(help)
+				fatal("* follows empty format\n" + help)
+			}
+			if len(a) > 1 {
+				fatal("* must be last in format\n" + help)
 			}
 			rem = f[len(f)-1]
 		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
@@ -84,23 +93,49 @@ func main() {
 				fatal("?")
 			}
 			if len(f) == 0 {
-				fatal(help)
+				fatal("number must follow a format\n" + help)
 			}
-			for i := 0; i < n; i++ {
+			for i := 0; i < n-1; i++ {
 				f = append(f, f[len(f)-1])
 			}
+			a = a[e:]
 		default:
-			fatal(help)
+			fatal("unknown token: " + string(a[0]) + "\n" + help)
 		}
 	}
+	headers()
+	// show()
 	read()
 }
+func headers() { // remove hhh from f(format)
+	var ff []byte
+	for _, c := range f {
+		if c == 'h' {
+			hdr++
+		} else {
+			ff = append(ff, c)
+		}
+	}
+	f = ff
+}
 func read() {
+	debom := func(s string) string { // remove byte-order-mark
+		if len(s) >= 3 && s[0] == 0xEF && s[1] == 0xBB && s[2] == 0xBF {
+			return s[3:]
+		}
+		return s
+	}
 	scn := bufio.NewScanner(os.Stdin)
 	num := -1
 	for scn.Scan() {
 		linenumber++
 		t := scn.Text()
+		if linenumber == 1 {
+			t = debom(t)
+		}
+		if len(t) > 0 && t[len(t)-1] == '\r' {
+			t = t[:len(t)-1]
+		}
 		if hdr > 0 {
 			hdr--
 			continue
@@ -113,10 +148,12 @@ func read() {
 		}
 		if num < 0 {
 			num = len(v)
-		} else if len(v) == 0 {
+		} else if len(v) == 0 || (len(v) == 1 && v[0] == "") {
 			continue // last line may be empty
 		} else if len(v) != num {
-			fatal("csv: input is not rectangular")
+			es := fmt.Sprintf("line %d: %d cols (not %d)", linenumber, len(v), num)
+			fmt.Printf("t: %q\n", t)
+			fatal("csv: input is not rectangular\n" + es)
 		}
 		write(v)
 	}
@@ -134,17 +171,19 @@ func write(v []string) {
 			format(v, i, rem)
 		}
 	}
+	fmt.Print("\n")
 }
 func format(v []string, i int, c byte) {
 	switch c {
 	case 'f':
 		fmt.Print(atof(v, i))
-	case 'z', '@':
+	case 'a', '@':
 		a, b := atof(v, i), atof(v, i+1)
-		if b[0] == '-' {
-			n, _ := strconv.ParseFloat(b, 64)
-			b = strconv.FormatFloat(n+360, 'f', -1, 64)
+		n, _ := strconv.ParseFloat(b, 64)
+		if n < 0 {
+			n += 360.0
 		}
+		b = strconv.FormatFloat(n, 'f', -1, 64) // no E in angle
 		fmt.Printf("%v%c%v", a, c, b)
 	case '-':
 	case 's':
@@ -172,5 +211,13 @@ func na() string {
 	panic("not enough columns for format. line " + strconv.Itoa(linenumber))
 }
 func fatal(s string) { fmt.Fprintln(os.Stderr, s); os.Exit(1) }
+func show() {
+	fmt.Println("ad(@):", ad)
+	fmt.Println("hdr:", hdr)
+	fmt.Println("nan(continue):", nan)
+	fmt.Println("rem(*):", string(rem))
+	fmt.Println("sep:", string(sep))
+	fmt.Println("f(format):", string(f))
+}
 
 var linenumber int
