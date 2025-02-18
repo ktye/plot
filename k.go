c86288b560e0bd7c929591e57f06e2992d8748e2
elmar <ktye@users.noreply.github.com>
2025-02-18 22:13:32 +0100

    kplot

--- k.go
+++ k.go
@@ -1,7 +1,158 @@
 package plot
 
-import "fmt"
+import (
+	//"fmt"
+	"bytes"
+	"strconv"
+	"strings"
+	//"math"
+	"encoding/binary"
+)
 
+func K(b []byte, x uint64, plts Plots, p int) (Plots, int, string) {
+	br := func(x uint64, data interface{}) {
+		binary.Read(bytes.NewReader(b[int32(x):]), binary.LittleEndian, data)
+	}
+	ux := func(p int32) uint64 { return binary.LittleEndian.Uint64(b[p:]) }
+	ix := func(p int32) int32 { return int32(binary.LittleEndian.Uint32(b[p:])) }
+	//fx := func(p int32) float64 { return math.Float64frombits(binary.LittleEndian.Uint64(b[p:])) }
+	//zx := func(p int32) complex128 { return complex(fx(p), fx(p+8)) }
+	nn := func(x uint64) int32 { return ix(int32(x) - 12) }
+	CK := func(x uint64) []byte { return b[int32(x) : int32(x)+nn(x)] }
+	IK := func(x uint64) []int32 { r := make([]int32, nn(x)); br(x, r); return r }
+	UK := func(x uint64) []uint64 { r := make([]uint64, nn(x)); br(x, r); return r }
+	FK := func(x uint64) []float64 { r := make([]float64, nn(x)); br(x, r); return r }
+	ZK := func(x uint64) []complex128 { r := make([]complex128, nn(x)); br(x, r); return r }
+	tp := func(x uint64) int { return int(x >> 59) }
+	s0 := ix(0)
+	sK := func(x uint64) string { return string(CK(ux(s0 + int32(x)))) }
+	SK := func(x uint64) []string {
+		v := IK(x)
+		r := make([]string, len(v))
+		for i, j := range v {
+			r[i] = string(CK(ux(s0 + j)))
+		}
+		return r
+	}
+	floats := func(x uint64) []float64 {
+		if tp(x) == 19 {
+			v := IK(x)
+			r := make([]float64, len(v))
+			for i := range v {
+				r[i] = float64(v[i])
+			}
+			return r
+		} else {
+			return FK(x)
+		}
+	}
+	axis := func(s []string) { plts[p].Limits, _ = AxisFromString(strings.Join(s, ",")) }
+	data := func(x []uint64) {
+		if len(x) == 0 {
+			return
+		}
+		var l Line
+		var X []float64
+		t0 := tp(x[0])
+		if t0 == 19 || t0 == 21 {
+			X = floats(x[0])
+		} else if t0 == 22 {
+			l.C = ZK(x[0])
+			plts[p].Lines = append(plts[p].Lines, l)
+			l = Line{}
+		}
+		push := func(n int) {
+			if X != nil {
+				if len(X) != n {
+					return
+				}
+				x := make([]float64, len(X))
+				copy(x, X)
+				l.X = x
+			}
+			plts[p].Lines = append(plts[p].Lines, l)
+			l = Line{}
+		}
+		for _, xi := range x[1:] {
+			if t0 == 19 || t0 == 21 {
+				l.Y = floats(xi)
+				push(len(l.Y))
+			} else if t0 == 22 {
+				l.C = ZK(xi)
+				push(len(l.C))
+			}
+		}
+	}
+
+	if len(plts) == 0 {
+		plts = append(plts, Plot{})
+	}
+
+	tx, xi := tp(x), int(int32(x))
+	switch tx {
+	case 0: //reset
+		return nil, 0, ""
+	case 2:
+		return plts, p, "todo: styles"
+	case 3:
+		if xi < 0 {
+			return nil, 0, ""
+		}
+		if xi == len(plts) {
+			p = xi
+			plts = append(plts, Plot{})
+		} else if xi < len(plts) {
+			p = xi
+		} else {
+			return plts, p, "plot id does not exist"
+		}
+	case 4:
+		s := sK(x) //todo equal
+		plts[p].Type = PlotType(s)
+	case 5:
+		return plts, p, "todo rand float"
+	case 6:
+		return plts, p, "todo rand polar"
+	case 18:
+		return plts, p, "todo styles"
+	case 19:
+		v := IK(x)
+		s := make([]string, len(v))
+		for i := range v {
+			s[i] = strconv.Itoa(int(v[i]))
+		}
+		axis(s)
+	case 20:
+		v := FK(x)
+		s := make([]string, len(v))
+		for i := range v {
+			s[i] = strconv.FormatFloat(v[i], 'g', -1, 64)
+		}
+		axis(s)
+	case 21:
+		v := SK(x)
+		if len(v) == 1 {
+			plts[p].Title = v[0]
+		} else if len(v) == 2 {
+			plts[p].Xlabel, plts[p].Ylabel = v[0], v[1]
+		} else if len(v) > 2 {
+			plts[p].Xlabel, plts[p].Ylabel, plts[p].Title = v[0], v[1], v[2]
+		}
+	case 22:
+		data([]uint64{21})
+	case 23:
+		data(UK(x))
+	case 24:
+		return plts, p, "todo key table plot"
+	case 25:
+		return plts, p, "todo table plot"
+	default:
+		return plts, p, "plot unknown k type"
+	}
+	return plts, p, ""
+}
+
+/*
 // KTablePlot creates plots from k tables.
 // It does not modify k memory.
 //
@@ -107,3 +258,4 @@ func nn(x uint32, I []uint32) (xn uint32) {
 	}
 	return I[x>>2] & 536870911
 }
+*/
