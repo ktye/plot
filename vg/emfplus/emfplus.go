deeec8cf5233237f3d392e67ed96ee41c0b063eb
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2025-03-20 16:59:40 +0100

    emfplus

--- vg/emfplus/emfplus.go
+++ vg/emfplus/emfplus.go
@@ -4,24 +4,21 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-
-	"github.com/ktye/plot/vg/emf"
+	"math"
 )
 
 func New(w, h int) *File {
 	f := File{
-		Header: Header{
-			Type:         0x4001,
-			Flags:        1,
-			Size:         0x1c,
-			DataSize:     0x10,
-			Version:      0xdbc01002,
-			EmfPlusFlags: 0,   //1 << 31,
-			DpiX:         600, //96,
-			DpiY:         600, //96,
-		},
-		width:  w,
-		height: h,
+		Header: Header{Type: 1, Size: 108, Bounds1: 0, Bounds2: 0, Bounds3: 560, Bounds4: 420,
+			Frame1: 0, Frame2: 0, Frame3: 15343, Frame4: 11484, Signature: 1179469088, Version: 65536,
+			Bytes: 1228, Records: 34, Handles: 2, Reserved: 0,
+			NDesc: 0, OffDesc: 0, NPals: 0,
+			DevWidth: 1920, DevHeight: 1080, MilliX: 527, MilliY: 296,
+			PixelFormat: 0, OffPixel: 0, Bopengl: 0,
+			MicroX: 527000, MicroY: 296000},
+		width:   w,
+		height:  h,
+		objects: 1,
 	}
 	return &f
 }
@@ -29,13 +26,20 @@ func New(w, h int) *File {
 type File struct {
 	Header
 	Records       []Record
-	objects       uint32
 	width, height int
+	objects       uint8
 }
 type Header struct {
-	Type, Flags              uint16
-	Size, DataSize, Version  uint32
-	EmfPlusFlags, DpiX, DpiY uint32
+	Type, Size                         uint32
+	Bounds1, Bounds2, Bounds3, Bounds4 int32
+	Frame1, Frame2, Frame3, Frame4     int32
+	Signature, Version, Bytes, Records uint32
+	Handles, Reserved                  uint16
+	NDesc, OffDesc, NPals              uint32
+	DevWidth, DevHeight                uint32
+	MilliX, MilliY                     uint32
+	PixelFormat, OffPixel, Bopengl     uint32 //header extension1
+	MicroX, MicroY                     uint32 //header extension2
 }
 type Record struct {
 	Type  uint16
@@ -43,14 +47,85 @@ type Record struct {
 	Size  uint32
 	Data  []uint32
 }
+type Emf struct {
+	Type, Size uint32
+	Data       []uint32
+}
 
+func (f *File) Pen(linewidth int, color uint32) uint8 { //color: 0xaarrggbb
+	id := f.objects
+	lw := math.Float32bits(float32(linewidth))
+	f.push(Record{0x4008, 0x200 | uint16(id), 52 - 8, []uint32{40 - 8, 3686797314, 0, 0, 0, lw, 3686797314, 0, color /*4278219453*/}})
+	f.objects++
+	return id
+}
+func (f *File) Brush(color uint32) uint8 {
+	id := f.objects
+	f.push(Record{0x4008, 0x100 | uint16(id), 24, []uint32{12, 3686797314, 0, color}})
+	f.objects++
+	return id
+}
 func rect(left, top, right, bottom int16) (u1, u2 uint32) {
 	return uint32(left) | uint32(top)<<16, uint32(right) | uint32(bottom)<<16
 }
 
-func (f *File) Ellipse(left, top, right, bottom int16) {
+func (f *File) DrawEllipse(pen uint8, left, top, right, bottom int16) {
 	u1, u2 := rect(left, top, right, bottom)
-	f.push(Record{0x400f, 0x2, 0x14, []uint32{0x8, u1, u2}})
+	f.push(Record{0x400f, 0x4000 | uint16(pen), 0x14, []uint32{0x8, u1, u2}})
+}
+func (f *File) FillEllipse(color uint32, left, top, right, bottom int16) {
+	u1, u2 := rect(left, top, right, bottom)
+	f.push(Record{0x400e, 0xc000, 24, []uint32{12, color, u1, u2}})
+}
+func (f *File) DrawPolyline(pen uint8, closepath bool, x, y []int16) {
+	cp := uint16(0)
+	if closepath {
+		cp = uint16(0x6000)
+	}
+	count := uint32(len(x))
+	u := make([]uint32, count)
+	for i := range x {
+		u[i] = uint32(x[i]) | uint32(y[i])<<16
+	}
+	f.push(Record{0x400d, 0xc000 | cp | uint16(pen), 16 + 4*count, append([]uint32{4 + 4*count, count}, u...)})
+}
+func (f *File) FillPolygon(color uint32, x, y []int16) {
+	count := uint32(len(x))
+	u := make([]uint32, count)
+	for i := range x {
+		u[i] = uint32(x[i]) | uint32(y[i])<<16
+	}
+	f.push(Record{0x400c, 0xc000, 0x14 + 4*count, append([]uint32{8 + 4*count, color, count}, u...)})
+}
+func (f *File) LineSegments(pen uint8, x0, x1, y0, y1 []int16) { //multiple individual lines using a path.
+	pathid := f.objects
+	f.objects++
+	count := uint32(len(x0))
+	u := make([]uint32, 2*len(x0))
+	t := make([]byte, 2*len(x0))
+	for i := range x0 {
+		u[2*i+0] = uint32(x0[i]) | uint32(y0[i]<<16)
+		u[2*i+1] = uint32(x1[i]) | uint32(y1[i]<<16)
+		t[2*i+0] = 0
+		t[2*i+1] = 9
+	}
+	for len(t)%4 != 0 {
+		t = append(t, 0xbf)
+	}
+	v := make([]uint32, len(t)/4)
+	binary.Read(bytes.NewReader(t), binary.LittleEndian, v)
+
+	s := uint32(0)
+	data := []uint32{s, 3686797314, count, 24576}
+	data = append(data, u...)
+	data = append(data, v...)
+	s = uint32(4*len(data) - 4)
+	data[0] = s
+	f.push(Record{0x4008, 0x300 | uint16(pathid), 12 + s, data})
+	f.push(Record{0x4015, uint16(pathid), 16, []uint32{4, uint32(pen)}})
+	// pathpointtype [flags|type] 4bits|4bits --> 0 9 0 9 0 9
+	// flag: close subpath 0x08
+	// type: 0x00(start) 0x01(line)
 }
 
 func (f *File) push(x Record) {
@@ -59,29 +134,48 @@ func (f *File) push(x Record) {
 	}
 	f.Records = append(f.Records, x)
 }
+
 func (f *File) MarshallBinary() []byte {
-	var r []Record
-	for _, x := range f.Records {
-		r = append(r, x)
-	}
-	r = append(r, Record{0x4002, 0, 0xc, []uint32{0}}) //eof
+	r := make([]Record, 0, 3+len(f.Records))
+	r = append(r, Record{0x4001, 0x1, 28, []uint32{16, 3686797314, 1, 96, 96}}) //emfplus header: Emf+dual, 96dpi
+	r = append(r, Record{0x401e, 0x9, 12, []uint32{0}})                         //anti-alias
+	r = append(r, f.Records...)
+	r = append(r, Record{0x4002, 0x0, 12, []uint32{0}}) //emfplus-eof
 
-	m := emf.New(f.width, f.height)
-
-	u := make([]uint32, binary.Size(f.Header)/4)
-	var b bytes.Buffer
-	fatal(binary.Write(&b, binary.LittleEndian, f.Header))
-	fatal(binary.Read(&b, binary.LittleEndian, u))
-	m.EmfplusComment(u)
-
-	u = nil
+	n := 0
 	for _, x := range r {
-		u = append(u, uint32(x.Type)|uint32(x.Flags)<<16, x.Size)
-		u = append(u, x.Data...)
+		n += 2 + len(x.Data)
 	}
+	s := uint32(4 * (1 + n))
 
-	m.EmfplusComment(u)
-	return m.MarshallBinary()
+	var b bytes.Buffer
+	we := func(x Emf) {
+		binary.Write(&b, binary.LittleEndian, x.Type)
+		binary.Write(&b, binary.LittleEndian, x.Size)
+		for _, u := range x.Data {
+			binary.Write(&b, binary.LittleEndian, u)
+		}
+	}
+	we(Emf{0x46, 12 + s, []uint32{s, 726027589}})
+
+	for _, x := range r {
+		binary.Write(&b, binary.LittleEndian, x.Type)
+		binary.Write(&b, binary.LittleEndian, x.Flags)
+		binary.Write(&b, binary.LittleEndian, x.Size)
+		for _, u := range x.Data {
+			binary.Write(&b, binary.LittleEndian, u)
+		}
+	}
+	we(Emf{0xe, 20, []uint32{0, 16, 20}}) //emf-eof
+
+	h := f.Header
+	h.Bytes = uint32(len(b.Bytes()) + binary.Size(h))
+	h.Records = uint32(3)
+
+	var o bytes.Buffer
+	binary.Write(&o, binary.LittleEndian, h)
+	o.Write(b.Bytes())
+	return o.Bytes()
 }
 func fatal(e error) {
 	if e != nil {
--- vg/emfplus/emfplus_test.go
+++ vg/emfplus/emfplus_test.go
@@ -5,50 +5,184 @@ import (
 	"testing"
 )
 
-const writeToDisk = true
+// const writeToDisk = true
+//
+//	func TestEmf(t *testing.T) {
+//		example(t)
+//	}
+//
+//	func example(t *testing.T) {
+//		f := New(500, 500)
+//		f.push(Record{0x401f, 0x3, 12, []uint32{0}})
+//		f.push(Record{0x4030, 0x2, 16, []uint32{4, 1065353216}}) //page-scale: 1
+//		f.push(Record{0x4022, 0x4, 12, []uint32{0}})
+//		f.push(Record{0x401e, 0x9, 12, []uint32{0}})
+//		f.push(Record{0x4021, 0x7, 12, []uint32{0}})
+//
+//		f.push(Record{0x402a, 0x0, 36, []uint32{24, 975962800, 0, 0, 975962800, 1159137963, 1151722155}})
+//		f.push(Record{0x400a, 0x8000, 36, []uint32{24, 4284193749, 1, 0, 0, 1230978560, 1230978560}})
+//
+//		f.push(Record{0x4008, 0x200, 52, []uint32{40, 3686797314, 0, 144, 0, 1179021312, 1090519040, 0, 3686797314, 0, 4282479004}})
+//		f.push(Record{0x4008, 0x301, 60, []uint32{48, 3686797314, 4, 0, 0, 0, 1230978560, 0, 1230978560, 1230978560, 0, 1230978560, 2164326656}})
+//		f.push(Record{0x4015, 0x1, 16, []uint32{4, 0}})
+//		//f.Ellipse(10, 10, 100, 100)
+//		/*
+//			f.AntiAlias()
+//			f.CreatePen(Pen{Width: 5, Color: Red})
+//			f.CreateBrush(Brush{Color: Blue})
+//			f.Select(0)
+//			f.Rectangle(10, 10, 410, 210)
+//			//f.SetTextColor(0)
+//			f.CreateFont(Font{Height: -20, Weight: 400, Face: "Consolas"})
+//			f.Select(2)
+//			f.Text(100, 100, "Hello 123-xyz")
+//			f.MoveTo(0, 0)
+//			f.LineTo(500, 250)
+//			f.Polyline([]int16{100, 200, 300, 400}, []int16{500, 400, 500, 400})
+//			f.Select(1)
+//			f.Ellipse(10, 10, 100, 100)
+//		*/
+//		f.write(t, "example.emf")
+//	}
+//
+//	func (f *File) write(t *testing.T, file string) {
+//		b := f.MarshallBinary()
+//		if writeToDisk {
+//			e := os.WriteFile(file, b, 0744)
+//			if e != nil {
+//				t.Fatal(e)
+//			}
+//		}
+//	}
 
-func TestEmf(t *testing.T) {
-	example(t)
-}
-func example(t *testing.T) {
-	f := New(500, 500)
-	f.push(Record{0x401f, 0x3, 12, []uint32{0}})
-	f.push(Record{0x4030, 0x2, 16, []uint32{4, 1065353216}}) //page-scale: 1
-	f.push(Record{0x4022, 0x4, 12, []uint32{0}})
-	f.push(Record{0x401e, 0x9, 12, []uint32{0}})
-	f.push(Record{0x4021, 0x7, 12, []uint32{0}})
+func TestEmfPlus(t *testing.T) {
+	f := New(0, 0)
+	f.Records = []Record{
+		//Record{0x4030, 0x2, 16, []uint32{4, 1065353216}}, //page transform: f32(1.0)
+		//Record{0x401e, 0x5, 12, []uint32{0}},                                                            //anti-alias
+		//Record{0x4008, 0x400, 40, []uint32{28, 3686797314, 0, 268435456, 0, 0, 1141637120, 1137836032}}, //object
+		//Record{0x4034, 0x0, 12, []uint32{0}},                                                            //clip-region
+		//Record{0x401e, 0x0, 12, []uint32{0}},                                                            //anti-alias
+		//Record{0x400a, 0xc000, 28, []uint32{16, 4294967295, 1, 0, 27525680}},                            //fill-rects
+		//Record{0x4031, 0x0, 12, []uint32{0}},                                 //reset clip
+		//Record{0x401e, 0x6, 12, []uint32{0}},                                 //anti-alias
+		//Record{0x400a, 0xc000, 28, []uint32{16, 4294967295, 1, 0, 27525680}}, //fill rects
 
-	f.push(Record{0x402a, 0x0, 36, []uint32{24, 975962800, 0, 0, 975962800, 1159137963, 1151722155}})
-	f.push(Record{0x400a, 0x8000, 36, []uint32{24, 4284193749, 1, 0, 0, 1230978560, 1230978560}})
+		//Record{0x401e, 0x9, 12, []uint32{0}}, //anti-alias
 
-	f.push(Record{0x4008, 0x200, 52, []uint32{40, 3686797314, 0, 144, 0, 1179021312, 1090519040, 0, 3686797314, 0, 4282479004}})
-	f.push(Record{0x4008, 0x301, 60, []uint32{48, 3686797314, 4, 0, 0, 0, 1230978560, 0, 1230978560, 1230978560, 0, 1230978560, 2164326656}})
-	f.push(Record{0x4015, 0x1, 16, []uint32{4, 0}})
-	//f.Ellipse(10, 10, 100, 100)
-	/*
-		f.AntiAlias()
-		f.CreatePen(Pen{Width: 5, Color: Red})
-		f.CreateBrush(Brush{Color: Blue})
-		f.Select(0)
-		f.Rectangle(10, 10, 410, 210)
-		//f.SetTextColor(0)
-		f.CreateFont(Font{Height: -20, Weight: 400, Face: "Consolas"})
-		f.Select(2)
-		f.Text(100, 100, "Hello 123-xyz")
-		f.MoveTo(0, 0)
-		f.LineTo(500, 250)
-		f.Polyline([]int16{100, 200, 300, 400}, []int16{500, 400, 500, 400})
-		f.Select(1)
-		f.Ellipse(10, 10, 100, 100)
-	*/
-	f.write(t, "example.emf")
-}
-func (f *File) write(t *testing.T, file string) {
+		//     object  pen                     version     0  flag unit w:0.6666  join/off   brush     solid aarrggbb:ff0072bd
+		//Record{0x4008, 0x201, 52, []uint32{40, 3686797314, 0, 136, 0, 1059760811, 2, 0, 3686797314, 0, 4278219453}}, //object pen(2)
+		//          c(type)(id)
+		//              2   1
+
+		//     object  path                    version     #  flags  point     point    align/pad
+		//Record{0x4008, 0x302, 36, []uint32{24, 3686797314, 2, 24576, 24510537, 2032123, 256}}, //object path(3)
+		//Record{0x4015, 0x2, 16, []uint32{4, 1}},                                               //draw-path
+	}
+
+	f.Pen(8, 0xffaa0000)
+	//f.DrawEllipse(1, 50, 50, 200, 200)
+	//f.DrawPolyline(1, true, []int16{0, 100, 200, 300}, []int16{0, 50, 0, 70})
+	f.LineSegments(1, []int16{0, 100, 200}, []int16{0, 100, 200}, []int16{0, 0, 0}, []int16{200, 200, 200}) //todo
+
+	f.FillPolygon(0xffff0000, []int16{100, 200, 300, 400, 400, 300, 200, 100}, []int16{100, 200, 100, 200, 400, 300, 400, 300})
+
+	//f.Brush(0xff00ff00)
+	//f.FillEllipse(0xff00ff00, 50, 50, 200, 200)
+
 	b := f.MarshallBinary()
-	if writeToDisk {
-		e := os.WriteFile(file, b, 0744)
-		if e != nil {
-			t.Fatal(e)
+	os.WriteFile("example.emf", b, 0744)
+}
+
+/*
+func TestEmfPlus1(t *testing.T) {
+	var o bytes.Buffer
+	erecs, precs := 0, 0
+
+	f := func(e Emf) {
+		erecs++
+		binary.Write(&o, binary.LittleEndian, e.Type)
+		binary.Write(&o, binary.LittleEndian, e.Size)
+		for _, u := range e.Data {
+			binary.Write(&o, binary.LittleEndian, u)
 		}
 	}
+	p := func(r Record) {
+		precs++
+		binary.Write(&o, binary.LittleEndian, r.Type)
+		binary.Write(&o, binary.LittleEndian, r.Flags)
+		binary.Write(&o, binary.LittleEndian, r.Size)
+		for _, u := range r.Data {
+			binary.Write(&o, binary.LittleEndian, u)
+		}
+	}
+
+	embed := func(r []Record) {
+		n := 0
+		for _, ri := range r {
+			n += 2 + len(ri.Data)
+		}
+		s := uint32(4 * (1 + n))
+		f(Emf{0x46, 12 + s, []uint32{s, 726027589}})
+		for _, x := range r {
+			p(x)
+		}
+	}
+
+	h := EmfHeader{Type: 1, Size: 108, Bounds1: 0, Bounds2: 0, Bounds3: 560, Bounds4: 420,
+		Frame1: 0, Frame2: 0, Frame3: 15343, Frame4: 11484, Signature: 1179469088, Version: 65536,
+		Bytes: 1228, Records: 34, Handles: 2, Reserved: 0,
+		NDesc: 0, OffDesc: 0, NPals: 0,
+		DevWidth: 1920, DevHeight: 1080, MilliX: 527, MilliY: 296,
+		PixelFormat: 0, OffPixel: 0, Bopengl: 0,
+		MicroX: 527000, MicroY: 296000}
+
+	//f(Emf{0x46, 44 + 304, []uint32{32 + 304, 726027589}})
+	embed([]Record{
+		Record{0x4001, 0x1, 28, []uint32{16, 3686797314, 1, 96, 96}}, //emfplus header: Emf+dual, 96dpi
+
+		Record{0x4030, 0x2, 16, []uint32{4, 1065353216}}, //page transform: f32(1.0)
+		//Record{0x401e, 0x5, 12, []uint32{0}},                                                            //anti-alias
+		//Record{0x4008, 0x400, 40, []uint32{28, 3686797314, 0, 268435456, 0, 0, 1141637120, 1137836032}}, //object
+		//Record{0x4034, 0x0, 12, []uint32{0}},                                                            //clip-region
+		//Record{0x401e, 0x0, 12, []uint32{0}},                                                            //anti-alias
+		//Record{0x400a, 0xc000, 28, []uint32{16, 4294967295, 1, 0, 27525680}},                            //fill-rects
+		//Record{0x4031, 0x0, 12, []uint32{0}},                                 //reset clip
+		//Record{0x401e, 0x6, 12, []uint32{0}},                                 //anti-alias
+		//Record{0x400a, 0xc000, 28, []uint32{16, 4294967295, 1, 0, 27525680}}, //fill rects
+
+		Record{0x401e, 0x9, 12, []uint32{0}}, //anti-alias
+
+		//     object  pen                     version     0  flag unit w:0.6666  join/off   brush     solid aarrggbb:ff0072bd
+		Record{0x4008, 0x201, 52, []uint32{40, 3686797314, 0, 136, 0, 1059760811, 2, 0, 3686797314, 0, 4278219453}}, //object pen(2)
+
+		//     object  path                    version     #  flags  point     point    align/pad
+		Record{0x4008, 0x302, 36, []uint32{24, 3686797314, 2, 24576, 24510537, 2032123, 256}}, //object path(3)
+		Record{0x4015, 0x2, 16, []uint32{4, 1}},                                               //draw-path
+		Record{0x4002, 0x0, 12, []uint32{0}},                                                  //eof
+	})
+	f(Emf{0xe, 20, []uint32{0, 16, 20}})
+
+	h.Bytes = uint32(len(o.Bytes()) + binary.Size(h))
+	h.Records = uint32(1 + erecs)
+
+	var w bytes.Buffer
+	binary.Write(&w, binary.LittleEndian, h)
+	o.WriteTo(&w)
+
+	os.WriteFile("example1.emf", w.Bytes(), 0744)
 }
+
+type EmfHeader struct {
+	Type, Size                         uint32
+	Bounds1, Bounds2, Bounds3, Bounds4 int32
+	Frame1, Frame2, Frame3, Frame4     int32
+	Signature, Version, Bytes, Records uint32
+	Handles, Reserved                  uint16
+	NDesc, OffDesc, NPals              uint32
+	DevWidth, DevHeight                uint32
+	MilliX, MilliY                     uint32
+	PixelFormat, OffPixel, Bopengl     uint32 //header extension1
+	MicroX, MicroY                     uint32 //header extension2
+}
+*/
